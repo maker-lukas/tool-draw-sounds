c849d2b1fb7918c110dc46cebc02ed9f89d0f96b
Lukas <itslukasreis@gmail.com>
2026-05-16 14:53:01 +0100

    inistilaze repo and add template

---
+++ src/client/java/com/orangopontotango/client/ToolDrawSoundsClient.java
@@ -0,0 +1,10 @@
+package com.orangopontotango.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToolDrawSoundsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
